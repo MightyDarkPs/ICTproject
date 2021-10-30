d436418f1f2e9d60bddd53ad77d87d64ae407937
Turlykhan <86224106+MightyDarkPs@users.noreply.github.com>
2021-10-30 22:23:14 +0600

    Update ICTproject.py

--- ICTproject.py
+++ ICTproject.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import requests
 
-st.set_page_config(page_title="KZT currency statistics", layout="wide")
+st.set_page_config(page_title="KZT currency statistics", layout="wide", page_icon="icon.png")
 
 image = Image.open('kzt.png')
 
